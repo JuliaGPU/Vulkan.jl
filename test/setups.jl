277e53ed8dbf707f28779a91bc7cb5e03da76f48
serenity4 <cedric.bel@hotmail.fr>
2020-10-01 22:24:18 +0200

    Use vertex indexing

--- test/setups.jl
+++ test/setups.jl
@@ -134,3 +134,22 @@ mutable struct BufferSetup <: Setup
         finalizer(x -> finalize.(getproperty.(x, [:handle, :memory])), bs)
     end
 end
+
+function Base.copyto!(dest::BufferSetup, src, device::DeviceSetup; size=WHOLE_SIZE)
+    ptr_ref = Ref{Ptr{Cvoid}}()
+    #TODO: Make a Ref{Ptr{Cvoid}} rather than Ref{Cvoid} for ppData
+    @check vkMapMemory(device, dest.memory, 0, size, 0, ptr_ref)
+    ptr_deref = ptr_ref[]
+    GC.@preserve src unsafe_copyto!(reinterpret(Ptr{eltype(src)}, ptr_deref), pointer(src), length(src))
+    unmap_memory(device, dest.memory)
+end
+
+function Base.copyto!(dest::BufferSetup, src::BufferSetup, size, device::DeviceSetup, command_pool)
+    cmd_buffer = first(CommandBuffer(device, CommandBufferAllocateInfo(command_pool, COMMAND_BUFFER_LEVEL_PRIMARY, 1)))
+    begin_command_buffer(cmd_buffer, CommandBufferBeginInfo(flags=COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT))
+    cmd_copy_buffer(cmd_buffer, src, dest, [BufferCopy(0, 0, size)])
+    end_command_buffer(cmd_buffer)
+    queue_submit(device.queues.graphics, [SubmitInfo([], [], [cmd_buffer], [])])
+    queue_wait_idle(device.queues.graphics)
+    free_command_buffers(device, command_pool, [cmd_buffer])
+end
