e71d07f02157177f4875e092f174a381bc047be5
serenity4 <cedric.bel@hotmail.fr>
2020-11-29 20:22:27 +0100

    Refactor script

--- generator/scripts/generate_library.jl
+++ generator/scripts/generate_library.jl
@@ -1,32 +1,34 @@
 using VulkanGen
 using VulkanCore
 
+getfile(name) = joinpath(dirname(dirname(pathof(VulkanCore))), "gen", name)
+
 const vg = VulkanGen
-
-getfile(name) = dirname(dirname(pathof(VulkanCore))) *  "/gen/" * name
-files = getfile.(["vk_common.jl", "vk_api.jl"])
-api = API(files, sym -> @eval(VulkanCore, $(Meta.parse("$sym"))))
-
-ignored_symbols = Dict(
-    "structs" => [
+const destfile = joinpath(dirname(dirname(@__DIR__)), "generated", "wrapped_api.jl")
+const files = getfile.(["vk_common.jl", "vk_api.jl"])
+const ignored_symbols = Dict(
+    :structs => [
         "VkBaseOutStructure",
         "VkBaseInStructure",
     ],
 )
 
-destfile = joinpath(dirname(dirname(@__DIR__)), "generated", "wrapped_api.jl")
-
 function filter_api(api)
     new_api = deepcopy(api)
     for (type, list) ∈ ignored_symbols, name ∈ list
-        api_dict = getproperty(new_api, Symbol(type))
-        haskey(api_dict, name) && delete!(api_dict, name)
+        api_dict = getproperty(new_api, type)
+        delete!(api_dict, name)
     end
     new_api
 end
 
-f_api = filter_api(api)
-w_api = wrap(f_api)
+function generate_wrapper(api)
+    f_api = filter_api(api)
+    w_api = wrap(f_api)
 
-@info "Writing wrapped API to $destfile."
-write(w_api, destfile)
+    @info "Writing wrapper to $destfile."
+    write(w_api, destfile)
+end
+
+api = API(files, sym -> Base.eval(VulkanCore, Meta.parse(sym)))
+generate_wrapper(api)
