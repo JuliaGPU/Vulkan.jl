fa7fd316f2972856f2afcc2d158b92834e370b85
serenity4 <cedric.bel@hotmail.fr>
2020-11-30 14:50:58 +0100

    Refactor includes #2

--- generator/src/vulkan/spec.jl
+++ generator/src/vulkan/spec.jl
@@ -5,13 +5,11 @@ using Vulkan_Headers_jll:vk_xml
 using DataFrames
 using Query
 
-spec_include(x...) = include(joinpath(@__DIR__, "spec", x...))
-
 xdoc = readxml(vk_xml)
 xroot = xdoc.root
 
-spec_include("utils.jl")
-spec_include("aliases.jl")
-spec_include("dataframes.jl")
-spec_include("graph.jl")
-spec_include("structure_types.jl")
+include("spec/utils.jl")
+include("spec/aliases.jl")
+include("spec/dataframes.jl")
+include("spec/graph.jl")
+include("spec/structure_types.jl")
