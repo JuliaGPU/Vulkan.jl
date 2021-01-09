6ad122b83f6bd0721aef2e194ae281ed9f1587aa
serenity4 <cedric.bel@hotmail.fr>
2021-01-09 21:25:43 +0100

    GC-root VulkanStruct dependencies
    Only cconverted variables are preserved from the GC. Previously, cconvert returned only .vks fields,
    so the .deps fields were not preserved and could be GC'd before the function call returns, potentially invalidating
    pointers present in the .vks field

--- generator/src/wrapper/prewrap.jl
+++ generator/src/wrapper/prewrap.jl
@@ -15,12 +15,14 @@ Represents a structure that will never be requested by API functions.
 abstract type ReturnedOnly <: VulkanStruct end
 
 Base.cconvert(T::Type, x::VulkanStruct) = x
-Base.cconvert(T::Type{<: Ptr}, x::AbstractArray{<: VulkanStruct}) = getproperty.(x, :vks)
-Base.cconvert(T::Type{<: Ptr}, x::AbstractArray{<: Handle}) = getproperty.(x, :handle)
-Base.cconvert(T::Type{<: Ptr}, x::VulkanStruct) = Ref(x.vks)
+Base.cconvert(T::Type{<: Ptr}, x::AbstractVector{<: VulkanStruct}) = (x, getproperty.(x, :vks))
+Base.cconvert(T::Type{<: Ptr}, x::AbstractVector{<: Handle}) = getproperty.(x, :handle)
+Base.cconvert(T::Type{<: Ptr}, x::VulkanStruct) = (x, Ref(x.vks))
 Base.cconvert(T::Type{<: Ptr}, x::Handle) = x
 
 Base.unsafe_convert(T::Type, x::VulkanStruct) = x.vks
+Base.unsafe_convert(T::Type, x::Tuple{<:VulkanStruct, <:Ref}) = Base.unsafe_convert(T, last(x))
+Base.unsafe_convert(T::Type, x::Tuple{<:AbstractVector{<:VulkanStruct}, <:Any}) = Base.unsafe_convert(T, last(x))
 Base.unsafe_convert(T::Type{Ptr{Nothing}}, x::Handle) = x.handle
 
-Base.broadcastable(x::VulkanStruct) = Ref(x) # indicate that VulkanStructs behave as scalars for broadcasting
+Base.broadcastable(x::VulkanStruct) = Ref(x)
