61c647afad226e864a00e91c71f7466ef095b6e0
serenity4 <cedric.bel@hotmail.fr>
2021-01-11 23:04:17 +0100

    Expose the presence of dependencies as a boolean type parameter

--- generator/src/wrapper/prewrap.jl
+++ generator/src/wrapper/prewrap.jl
@@ -1,28 +1,32 @@
 """
 Represents any kind of wrapper structure that was generated from a Vulkan structure.
+`D` is a `Bool` parameter indicating whether the structure has specific dependencies or not.
 """
-abstract type VulkanStruct end
+abstract type VulkanStruct{D} end
 
 """
 Opaque handle referring to internal Vulkan data.
 Finalizer registration is taken care of by constructors.
 """
-abstract type Handle <: VulkanStruct end
+abstract type Handle <: VulkanStruct{false} end
 
 """
 Represents a structure that will never be requested by API functions.
 """
-abstract type ReturnedOnly <: VulkanStruct end
+abstract type ReturnedOnly <: VulkanStruct{false} end
 
 Base.cconvert(T::Type, x::VulkanStruct) = x
-Base.cconvert(T::Type{<:Ptr}, x::AbstractVector{<:VulkanStruct}) = (x, getproperty.(x, :vks))
-Base.cconvert(T::Type{<:Ptr}, x::AbstractVector{<:Handle}) = getproperty.(x, :handle)
-Base.cconvert(T::Type{<:Ptr}, x::VulkanStruct) = (x, Ref(x.vks))
+Base.cconvert(T::Type{<:Ptr}, x::AbstractVector{<:VulkanStruct{false}}) = getproperty.(x, :vks)
+Base.cconvert(T::Type{<:Ptr}, x::AbstractVector{<:VulkanStruct{true}}) = (x, getproperty.(x, :vks))
+Base.cconvert(T::Type{<:Ptr}, x::VulkanStruct{false}) = Ref(x.vks)
+Base.cconvert(T::Type{<:Ptr}, x::VulkanStruct{true}) = (x, Ref(x.vks))
 Base.cconvert(T::Type{<:Ptr}, x::Handle) = x
 
 Base.unsafe_convert(T::Type, x::VulkanStruct) = x.vks
-Base.unsafe_convert(T::Type, x::Tuple{<:VulkanStruct, <:Ref}) = Base.unsafe_convert(T, last(x))
-Base.unsafe_convert(T::Type, x::Tuple{<:AbstractVector{<:VulkanStruct}, <:Any}) = Base.unsafe_convert(T, last(x))
-Base.unsafe_convert(T::Type{Ptr{Nothing}}, x::Handle) = x.handle
+Base.unsafe_convert(T::Type, x::Tuple{<:VulkanStruct{true}, <:Ref}) = Base.unsafe_convert(T, last(x))
+Base.unsafe_convert(T::Type, x::Tuple{<:AbstractVector{<:VulkanStruct{true}}, <:Any}) = Base.unsafe_convert(T, last(x))
 
 Base.broadcastable(x::VulkanStruct) = Ref(x)
+
+from_vk(T::Type{<:VulkanStruct{false}}, x) = T(x)
+from_vk(T::Type{<:VulkanStruct{true}}, x) = T(x, [])
