1f7f8e3480061ecea952397e943da9e46ce8b261
serenity4 <cedric.bel@hotmail.fr>
2021-01-11 23:09:47 +0100

    Remove deprecated functions
    The generated wrapper will no longer use them

--- src/wrap_utils.jl
+++ src/wrap_utils.jl
@@ -29,25 +29,15 @@ macro check(expr, msg)
     end
 end
 
-unsafe_pointer(obj::Base.RefValue{T}) where {T} = Base.unsafe_convert(Ptr{T}, obj)
-unsafe_pointer(obj::AbstractArray) = pointer(obj)
-unsafe_pointer(obj::AbstractString) = pointer(obj)
-unsafe_pointer(obj::Ptr{Nothing}) = obj
-
-unsafe_pointer_load(ptr::Ptr{T}; index=1) where {T} = unsafe_load(ptr, index)
-unsafe_pointer_load(ptr::Ptr{T}, length::Integer) where {T} = unsafe_wrap(Array{T}, ptr, length)
-unsafe_pointer_load(ptr::Ptr{UInt8}) = unsafe_string(ptr)
-unsafe_pointer_load(ptr::Cstring) = unsafe_string(ptr)
-
-pointer_length(p) = p == C_NULL || isempty(p) ? 0 : length(p)
+pointer_length(p::Ptr{Nothing}) = 0
+pointer_length(p::AbstractArray) = length(p)
 
 to_vk(T, x) = convert(T, x)
 to_vk(T::Type{UInt32}, version::VersionNumber) = T((version.major << 22) + (version.minor << 12) + version.patch)
-to_vk(T::Type{NTuple{N,UInt8}}, s::AbstractString) where {N} = T(s * "\0" ^ (N - length(s)))
+to_vk(T::Type{NTuple{N,UInt8}}, s::AbstractString) where {N} = T(s * '\0' ^ (N - length(s)))
 
 from_vk(T, x) = convert(T, x)
 from_vk(T::Type{VersionNumber}, version::UInt32) = T(version >> 22, (version >> 12) & 0x3ff, version & 0xfff)
-from_vk(T::Type{<: AbstractArray{P}}, b::Ptr{P}; length) where P = unsafe_pointer_load(b, length)
 from_vk(T::Type{S}, str::NTuple{N,UInt8}) where {N,S <: AbstractString} = T(filter!(x -> x ≠ 0, UInt8[str...]))
 
 bitwise_or_optional(value, option::Nothing) = value
