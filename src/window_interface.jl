96c2b14469472e2827d47e6145a5b3cfc998315c
serenity4 <cedric.bel@hotmail.fr>
2021-01-15 20:48:34 +0100

    Remove unwanted feature

--- src/window_interface.jl
+++
@@ -1,7 +0,0 @@
-using .WindowAbstractions: AbstractWindow
-
-function target!(app::VulkanApplication, target::AbstractWindow; format=FORMAT_B8G8R8A8_SRGB, swapchain_kwargs...)
-    add_surface!(app, SurfaceSetup(app, target))
-    create_swapchain!(app; format, swapchain_kwargs...)
-    prepare_rendering_to_target!(app, Target{SwapchainKHR}())
-end
