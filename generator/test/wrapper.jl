af02247a59c1494690e53e3e3f58bfe4f74dacf3
serenity4 <cedric.bel@hotmail.fr>
2021-01-16 11:11:20 +0100

    Remove usage test
    Testing Vulkan should be enough

--- generator/test/wrapper.jl
+++ generator/test/wrapper.jl
@@ -2,5 +2,4 @@
     include("wrapper/exprs.jl")
     include("wrapper/naming_conventions.jl")
     include("wrapper/wrap.jl")
-    @safetestset "Wrapper production and usage" begin include("wrapper/usage.jl") end
 end
