1d6fcd0ec820f0b3b02427b758da3425cdca0fcd
serenity4 <cedric.bel@hotmail.fr>
2021-01-18 20:39:52 +0100

    Rename variable

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 @static if get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "ON"
     using SwiftShader_jll: libvulkan
     ENV["JULIA_VULKAN_LIBNAME"] = basename(libvulkan)
-    libpath = dirname(libvulkan)
+    libdir = dirname(libvulkan)
 
     sep = Sys.iswindows() ? ';' : ':'
     old_vk_icd_filenames = get(ENV, "VK_ICD_FILENAMES", [])
-    ENV["VK_ICD_FILENAMES"] = join([old_vk_icd_filenames; joinpath(libpath, "vk_swiftshader_icd.json")], sep)
+    ENV["VK_ICD_FILENAMES"] = join([old_vk_icd_filenames; joinpath(libdir, "vk_swiftshader_icd.json")], sep)
 end
 
 
