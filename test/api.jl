dc77df137a11595c35295303dd267a8a1aa82703
serenity4 <cedric.bel@hotmail.fr>
2021-02-27 17:58:50 +0100

    Replace expression by its enum value

--- test/api.jl
+++ test/api.jl
@@ -48,7 +48,7 @@ end
 
     @testset "Bitmask flags" begin
         buffer = Buffer(device, 24, BUFFER_USAGE_INDIRECT_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE, [0]; flags=BUFFER_CREATE_PROTECTED_BIT & BUFFER_CREATE_SPARSE_ALIASED_BIT)
-        buffer_2 = Buffer(device, 24, BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT, VkSharingMode(0), [0]; flags=0)
+        buffer_2 = Buffer(device, 24, BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT, VK_SHARING_MODE_EXCLUSIVE, [0]; flags=0)
         @test buffer isa Buffer
         @test buffer_2 isa Buffer
     end
