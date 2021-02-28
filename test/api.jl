7431e0d3aac47a7bd141c0fc926a97b519c9bc8e
serenity4 <cedric.bel@hotmail.fr>
2021-02-28 18:55:32 +0100

    Adapt Vulkan tests
    `unwrap`s were removed from a few places where it is no longer necessary

--- test/api.jl
+++ test/api.jl
@@ -47,8 +47,8 @@ end
     end
 
     @testset "Bitmask flags" begin
-        buffer = unwrap(Buffer(device, 24, BUFFER_USAGE_INDIRECT_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE, [0]; flags=BUFFER_CREATE_PROTECTED_BIT & BUFFER_CREATE_SPARSE_ALIASED_BIT))
-        buffer_2 = unwrap(Buffer(device, 24, BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT, VK_SHARING_MODE_EXCLUSIVE, [0]; flags=0))
+        buffer = Buffer(device, 24, BUFFER_USAGE_INDIRECT_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE, [0]; flags=BUFFER_CREATE_PROTECTED_BIT & BUFFER_CREATE_SPARSE_ALIASED_BIT)
+        buffer_2 = Buffer(device, 24, BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT, VK_SHARING_MODE_EXCLUSIVE, [0]; flags=0)
         @test buffer isa Buffer
         @test buffer_2 isa Buffer
     end
--- test/init.jl
+++ test/init.jl
@@ -1,13 +1,13 @@
-@test_throws VulkanError("failed to execute vkCreateInstance(create_info, allocator, pInstance)", VK_ERROR_LAYER_NOT_PRESENT) unwrap(Instance(["nonexistent_layer"], String[]))
-@test_throws VulkanError("failed to execute vkCreateInstance(create_info, allocator, pInstance)", VK_ERROR_EXTENSION_NOT_PRESENT) unwrap(Instance(String[], ["nonexistent_extension"]))
-@test unwrap_error(Instance(String[], ["nonexistent_extension"])) == VulkanError("failed to execute vkCreateInstance(create_info, allocator, pInstance)", VK_ERROR_EXTENSION_NOT_PRESENT)
+@test_throws VulkanError("failed to execute vkCreateInstance(create_info, allocator, pInstance)", VK_ERROR_LAYER_NOT_PRESENT) Instance(["nonexistent_layer"], [])
+@test_throws VulkanError("failed to execute vkCreateInstance(create_info, allocator, pInstance)", VK_ERROR_EXTENSION_NOT_PRESENT) Instance([], ["nonexistent_extension"])
+@test unwrap_error(create_instance([], ["nonexistent_extension"])) == VulkanError("failed to execute vkCreateInstance(create_info, allocator, pInstance)", VK_ERROR_EXTENSION_NOT_PRESENT)
 
-const instance = unwrap(Instance(INSTANCE_LAYERS, INSTANCE_EXTENSIONS;
-    application_info = ApplicationInfo(v"0.0.1", v"0.0.1", API_VERSION; application_name="Test", engine_name="Experimental engine")))
+const instance = Instance(INSTANCE_LAYERS, INSTANCE_EXTENSIONS;
+    application_info = ApplicationInfo(v"0.0.1", v"0.0.1", API_VERSION; application_name="Test", engine_name="Experimental engine"))
 @info "Instance created: $instance."
 
 if WITH_VALIDATION[]
-    debug_messenger = unwrap(DebugUtilsMessengerEXT(instance, debug_callback_c; severity="debug"))
+    debug_messenger = DebugUtilsMessengerEXT(instance, debug_callback_c; severity="debug")
 end
 
 const device = let pdevices = unwrap(enumerate_physical_devices(instance))
@@ -23,11 +23,11 @@ const device = let pdevices = unwrap(enumerate_physical_devices(instance))
     @test_throws ErrorException("No queue with the desired capabilities could be found.") find_queue_index(pdevice, typemax(QueueFlag))
     @test_throws ErrorException("Invalid physical device features: no_feature") PhysicalDeviceFeatures(:no_feature)
 
-    unwrap(Device(
+    Device(
         pdevice,
         [DeviceQueueCreateInfo(find_queue_index(pdevice, QUEUE_GRAPHICS_BIT & QUEUE_COMPUTE_BIT), [1.0])],
         [], DEVICE_EXTENSIONS; enabled_features = ENABLED_FEATURES
-    ))
+    )
 end
 
 @info "Logical device created: $device."
