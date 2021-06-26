8e07667212223df7953af955713166e1a3fc1f0d
serenity4 <cedric.bel@hotmail.fr>
2021-06-26 15:56:38 +0200

    Fix config tests

--- generator/test/wrapper/generation/config.jl
+++ generator/test/wrapper/generation/config.jl
@@ -1,5 +1,7 @@
-@test isempty(extensions(WrapperConfig(wrap_core=false)))
-@test length(extensions(WrapperConfig())) > 200
-@test length(extensions(WrapperConfig(wrap_core=false, include_platforms=[PLATFORM_VI]))) < 5
-@test length(extensions(WrapperConfig(include_provisional_exts=true))) > length(extensions(WrapperConfig()))
-@test extensions(WrapperConfig(include_provisional_exts=true)) == extensions(WrapperConfig(include_platforms=[PLATFORM_PROVISIONAL]))
+wrapper_config(; kwargs...) = WrapperConfig(; destfile="", kwargs...)
+
+@test isempty(extensions(wrapper_config(wrap_core=false)))
+@test length(extensions(wrapper_config())) > 200
+@test length(extensions(wrapper_config(wrap_core=false, include_platforms=[PLATFORM_VI]))) < 5
+@test length(extensions(wrapper_config(include_provisional_exts=true))) > length(extensions(wrapper_config()))
+@test extensions(wrapper_config(include_provisional_exts=true)) == extensions(wrapper_config(include_platforms=[PLATFORM_PROVISIONAL]))
