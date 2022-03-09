c815d6a34ba14946009f9a67ac074f05cf1d75df
serenity4 <cedric.bel@hotmail.fr>
2022-03-09 18:38:30 +0100

    Fix bitmask printing

--- src/prewrap/bitmasks.jl
+++ src/prewrap/bitmasks.jl
@@ -43,7 +43,7 @@ macro bitmask_flag(typedecl, expr)
 end
 
 Base.iszero(a::BitMask) = iszero(a.val)
-Base.zero(a::T) where {T <: BitMask} = T(zero(a.val))
+Base.zero(a::T) where {T<:BitMask} = T(zero(a.val))
 Base.:(~)(a::T) where {T<:BitMask} = T(~a.val)
 
 (&)(a::BitMask, b::BitMask) = error("Bitwise operation not allowed between incompatible bitmasks '$(typeof(a))', '$(typeof(b))'")
@@ -53,47 +53,47 @@ isless(a::BitMask, b::BitMask) = error("Bitwise operation not allowed between in
 (==)(a::BitMask, b::BitMask) = error("Operation not allowed between incompatible bitmasks '$(typeof(a))', '$(typeof(b))'")
 in(a::BitMask, b::BitMask) = error("Operation not allowed between incompatible bitmasks '$(typeof(a))', '$(typeof(b))'")
 
-(&)(a::T, b::T) where {T <: BitMask} = T(a.val & b.val)
-(|)(a::T, b::T) where {T <: BitMask} = T(a.val | b.val)
-xor(a::T, b::T) where {T <: BitMask} = T(xor(a.val, b.val))
-isless(a::T, b::T) where {T <: BitMask} = isless(a.val, b.val)
-(==)(a::T, b::T) where {T <: BitMask} = a.val == b.val
-in(a::T, b::T) where {T <: BitMask} = a & b == a
+(&)(a::T, b::T) where {T<:BitMask} = T(a.val & b.val)
+(|)(a::T, b::T) where {T<:BitMask} = T(a.val | b.val)
+xor(a::T, b::T) where {T<:BitMask} = T(xor(a.val, b.val))
+isless(a::T, b::T) where {T<:BitMask} = isless(a.val, b.val)
+(==)(a::T, b::T) where {T<:BitMask} = a.val == b.val
+in(a::T, b::T) where {T<:BitMask} = a & b == a
 
-(&)(a::T, b::Integer) where {T <: BitMask} = T(a.val & b)
-(|)(a::T, b::Integer) where {T <: BitMask} = T(a.val | b)
-xor(a::T, b::Integer) where {T <: BitMask} = T(xor(a.val, b))
-isless(a::T, b::Integer) where {T <: BitMask} = isless(a.val, b)
-in(a::T, b::Integer) where {T <: BitMask} = a & b == a
+(&)(a::T, b::Integer) where {T<:BitMask} = T(a.val & b)
+(|)(a::T, b::Integer) where {T<:BitMask} = T(a.val | b)
+xor(a::T, b::Integer) where {T<:BitMask} = T(xor(a.val, b))
+isless(a::T, b::Integer) where {T<:BitMask} = isless(a.val, b)
+in(a::T, b::Integer) where {T<:BitMask} = a & b == a
 
-(&)(a::Integer, b::T) where {T <: BitMask} = b & a
-(|)(a::Integer, b::T) where {T <: BitMask} = b | a
-xor(a::Integer, b::T) where {T <: BitMask} = xor(b, a)
-isless(a::Integer, b::T) where {T <: BitMask} = isless(a, b.val) # need b.val to prevent stackoverflow
-in(a::Integer, b::T) where {T <: BitMask} = a | b == b
+(&)(a::Integer, b::T) where {T<:BitMask} = b & a
+(|)(a::Integer, b::T) where {T<:BitMask} = b | a
+xor(a::Integer, b::T) where {T<:BitMask} = xor(b, a)
+isless(a::Integer, b::T) where {T<:BitMask} = isless(a, b.val) # need b.val to prevent stackoverflow
+in(a::Integer, b::T) where {T<:BitMask} = a | b == b
 
-(::Type{T})(bm::BitMask) where {T <: Integer} = T(bm.val)
+(::Type{T})(bm::BitMask) where {T<:Integer} = T(bm.val)
 
 convert(T::Type{<:Integer}, bm::BitMask) = T(bm.val)
 convert(T::Type{<:BitMask}, val::Integer) = T(val)
 
 typemax(T::Type{<:BitMask{_T}}) where {_T} = T(typemax(_T))
 
-function Base.show(io::IO, flag::T) where {T <: BitMask}
+function Base.show(io::IO, flag::T) where {T<:BitMask}
     print(io, nameof(T), '(')
     first = true
     _flag = flag
 
     # print any combination of flags
     for (name, val) in values(T)
-        if Int(val) == 0 && flag == val
+        if iszero(val) && iszero(flag)
             print(io, name, ')')
             return
         end
         l2 = log2(Int(val))
-        if l2 % 1. ≠ 0.
+        if !iszero(l2 % 1.0)
             # the value is a combination of flags
-            if val in flag
+            if val in flag && !iszero(val)
                 !first && print(io, " | ")
                 first = false
                 print(io, name)
@@ -105,7 +105,7 @@ function Base.show(io::IO, flag::T) where {T <: BitMask}
     if !iszero(_flag)
         # print remaining flags
         for (name, val) in values(T)
-            if val in _flag && (Int(val) ≠ 0 || val == _flag)
+            if val in _flag && (!iszero(val) || val == _flag)
                 !first && print(io, " | ")
                 first = false
                 print(io, name)
