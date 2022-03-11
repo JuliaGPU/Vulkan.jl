f2e5a071f12850cf0c3e745ae3251c58e2447a43
Documenter.jl <documenter@juliadocs.github.io>
2022-03-11 11:34:11 +0000

    build based on c4b9224

---
+++ v0.6.0/tutorial/indepth.jl
@@ -0,0 +1,116 @@
+#=
+
+# In-depth tutorial
+
+This objective of this in-depth tutorial is to introduce the reader to the functionality of this library and reach a level of familiarity sufficient for building more complex applications.
+
+This tutorial is *not* intended as a replacement for a Vulkan tutorial. In particular, it is assumed that the reader has a basic understanding of Vulkan. If you are new to Vulkan, feel free to follow the [official Vulkan tutorial](https://vulkan-tutorial.com/) along with this one. The Vulkan tutorial will teach you the concepts behind Vulkan, and this tutorial how to use the API it *from Julia*.
+A lot of resources are available online for learning about Vulkan, such as the [Vulkan Guide](https://github.com/KhronosGroup/Vulkan-Guide) by the Khronos Group. You can find a more detailed list [here](https://www.vulkan.org/learn).
+
+## Initialization
+
+The entry point of any Vulkan application is an `Instance`, so we will create one. We will use validation layers and the extension `VK_EXT_debug_utils` that will allow logging from Vulkan. We will also provide an `ApplicationInfo` parameter to request the 1.2 version of the API.
+
+=#
+
+using SwiftShader_jll # hide
+using Vulkan
+@set_driver :SwiftShader # hide
+
+const application_info = ApplicationInfo(
+    v"0.0.1", # application version
+    v"0.0.1", # engine version
+    v"1.2"; # requested API version
+    application_name = "Demo",
+    engine_name = "DemoEngine",
+)
+
+#=
+
+The application and engine versions don't matter here, but must be provided regardless. We request the version 1.2 of the Vulkan API (ensure you have a driver compatible with version 1.2 first). Application and engine names won't matter either, but we provide them for demonstration purposes.
+
+```julia
+const instance = Instance(
+    ["VK_LAYER_KHRONOS_validation"],
+    ["VK_EXT_debug_utils"];
+    application_info,
+)
+```
+
+This simple call does a few things under the hood:
+- it creates an `InstanceCreateInfo` with the provided arguments
+- it calls `create_instance` with the create info, which in turn:
+  - calls the API constructor `vkCreateInstance`
+  - checks if an error occured; if so, return a `ResultTypes.Result` type wrapping an error
+  - registers a finalizer to a newly created `Instance` that will call `destroy_instance` (forwarding to `vkDestroyInstance`) when necessary
+- unwraps the result of `create_instance`, which is assumed to be a success code (otherwise an exception is thrown).
+
+Note that this little abstraction does not induce any loss of functionality. Indeed, the `Instance` constructor has a few keyword arguments not mentioned above for a more advanced use, which simply provides default values.
+
+Note that we pass in arrays, version numbers and strings; but the C API does not know anything about Julia types. Fortunately, these conversions are taken care of by the wrapper, so that we don't need to provide pointers for arrays and strings nor integers that act as version numbers.
+
+We now setup a debug messenger that we'll use for logging. Its function is to process messages sent by the Vulkan API. We could use the default debug callback provided by Vulkan.jl, namely [`default_debug_callback`](@ref); but instead we will implement our own callback for educational purposes. We'll just define a function that prints whatever message is received from Vulkan.
+
+We won't just `println`, because it does context-switching which is not allowed in finalizers (and the callback may be called in a finalizer, notably when functions like `vkDestroy...` are called). We can use `jl_safe_printf` which does not go through the Julia task system to safely print messages. The data that will arrive from Vulkan will be a `Ptr{VkCore.VkDebugUtilsMessengerCallbackDataEXT}`
+
+```julia
+function debug_callback(severity, type, p_data, p_user_data)
+    p_data ≠ C_NULL || return UInt32(0) # don't print if there's no message
+    data = unsafe_load(p_data)
+    msg = unsafe_string(data.pMessage)
+    ccall(:jl_safe_printf, Cstring, (Cstring,), string(msg, '\n'))
+    return UInt32(0)
+end
+```
+
+Because we are passing a callback to Vulkan as a function pointer, we need to convert it to a function pointer using `@cfunction`:
+
+```julia
+const debug_callback_c = @cfunction(
+    default_debug_callback,
+    UInt32,
+    (
+        DebugUtilsMessageSeverityFlagEXT,
+        DebugUtilsMessageTypeFlagEXT,
+        Ptr{VkCore.VkDebugUtilsMessengerCallbackDataEXT},
+        Ptr{Cvoid},
+    )
+)
+```
+
+!!! warning
+    If you intend to do this inside a module that will be precompiled, you must create the function pointer in the `__init__()` stage:
+    ```julia
+    const debug_callback_c = Ref{Ptr{Cvoid}}()
+    function __init__()
+        debug_callback_c[] = @cfunction(...) # the expression above
+    end
+    ```
+    This is because function pointers are only valid in a given runtime environment, so you need to get it at runtime (and *not* compile time).
+
+Note that the signature uses pointers and structures from VulkanCore.jl (accessible through the exported `core` and `vk` aliases). This is because we currently don't generate wrappers for defining function pointers. The flag types are still wrapper types, because the wrapped versions share the same binary representation as the core types. Let's create the debug messenger for all message types and severities:
+
+```julia
+const debug_messenger = DebugUtilsMessengerEXT(
+    instance,
+    |(
+        DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT,
+        DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT,
+        DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT,
+        DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+    ),
+    |(
+        DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT,
+        DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT,
+        DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+    ),
+    debug_callback_c,
+)
+```
+
+!!! note
+    `DebugUtilsMessengerEXT` is an extension-defined handle. Any extension function such as `vkCreateDebugUtilsMessengerEXT` and `vkDestroyDebugUtilsMessengerEXT` (called in the constructor and finalizer respectively) must be called using a function pointer. This detail is abstracted away with the wrapper, as API function pointers are automatically retrieved as needed and stored in a thread-safe global dispatch table. See more in the [Dispatch](@ref) section.
+
+We can now enumerate and pick a physical device that we will use for this tutorial.
+
+=#
---
+++ v0.6.0/tutorial/minimal_working_compute.jl
@@ -0,0 +1,314 @@
+
+#=
+
+# Minimal working compute example
+
+The amount of control offered by Vulkan is not a very welcome property for
+users who just want to run a simple shader to compute something quickly, and
+the effort required for the "first good run" is often quite deterrent. To ease
+the struggle, this tutorial gives precisely the small "bootstrap" piece of code
+that should allow you to quickly run a compute shader on actual data. In short,
+we walk through the following steps:
+
+- Opening a device and finding good queues families and memory types
+- Allocating memory and buffers
+- Compiling a shader program and filling up the structures necessary to run it:
+  - specialization constants
+  - push constants
+  - descriptor sets and layouts
+- Making a command buffer and submitting it to the queue, efficiently running
+  the shader
+
+## Initialization
+
+=#
+
+using SwiftShader_jll # hide
+using Vulkan
+@set_driver :SwiftShader # hide
+
+instance = Instance(["VK_LAYER_KHRONOS_validation"], [])
+
+# Take the first available physical device (you might check that it is an
+# actual GPU, using [`get_physical_device_properties`](@ref)).
+physical_device = first(unwrap(enumerate_physical_devices(instance)))
+
+# At this point, we need to choose a queue family index to use. For this
+# example, have a look at `vulkaninfo` command and pick the good queue manually
+# from the list of `VkQueueFamilyProperties` -- you want one that has
+# `QUEUE_COMPUTE` in the flags. In a production environment, you would use
+# [`get_physical_device_queue_family_properties`](@ref) to find a good index.
+qfam_idx = 0
+
+# Create a device object and make a queue for our purposes.
+device = Device(physical_device, [DeviceQueueCreateInfo(qfam_idx, [1.0])], [], [])
+
+# ## Allocating the memory
+#
+# Same way, you need to find a good memory type. Again, you can find a good one
+# using `vulkaninfo` or with [`get_physical_device_memory_properties`](@ref).
+# For compute, you want something that is both at the device (contains
+# `MEMORY_PROPERTY_DEVICE_LOCAL_BIT`) and visible from the host
+# (`..._HOST_VISIBLE_BIT`).
+memorytype_idx = 0
+
+# Let's create some data. We will work with 100 flimsy floats.
+data_items = 100
+mem_size = sizeof(Float32) * data_items
+
+# Allocate the memory of the correct type
+mem = DeviceMemory(device, mem_size, memorytype_idx)
+
+# Make a buffer that will be used to access the memory, and bind it to the
+# memory. (Memory allocations may be quite demanding, it is therefore often
+# better to allocate a single big chunk of memory, and create multiple buffers
+# that view it as smaller arrays.)
+buffer = Buffer(
+    device,
+    mem_size,
+    BUFFER_USAGE_STORAGE_BUFFER_BIT,
+    SHARING_MODE_EXCLUSIVE,
+    [qfam_idx],
+)
+
+bind_buffer_memory(device, buffer, mem, 0)
+
+# ## Uploading the data to the device
+#
+# First, map the memory and get a pointer to it.
+memptr = unwrap(map_memory(device, mem, 0, mem_size))
+
+# Here we make Julia to look at the mapped data as a vector of `Float32`s, so
+# that we can access it easily:
+data = unsafe_wrap(Vector{Float32}, convert(Ptr{Float32}, memptr), data_items, own = false);
+
+# For now, let's just zero out all the data, and *flush* the changes to make
+# sure the device can see the updated data. This is the simplest way to move
+# array data to the device.
+data .= 0
+unwrap(flush_mapped_memory_ranges(device, [MappedMemoryRange(mem, 0, mem_size)]))
+# The flushing is not required if you have verified that the memory is
+# host-coherent (i.e., has `MEMORY_PROPERTY_HOST_COHERENT_BIT`).
+
+# Eventually, you may need to allocate memory types that are not visible from
+# host, because these provide better capacity and speed on the discrete GPUs.
+# At that point, you may need to use the transfer queue and memory transfer
+# commands to get the data from host-visible to GPU-local memory, using e.g.
+# [`cmd_copy_buffer`](@ref).
+
+# ## Compiling the shader
+#
+# Now we need to make a shader program. We will use `glslangValidator` packaged
+# in a JLL to compile a GLSL program from a string into a spir-v bytecode,
+# which is later passed to the GPU drivers.
+
+shader_code = """
+#version 430
+
+layout(local_size_x_id = 0) in;
+layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+
+layout(constant_id = 0) const uint blocksize = 1; // manual way to capture the specialization constants
+
+layout(push_constant) uniform Params
+{
+    float val;
+    uint n;
+} params;
+
+layout(std430, binding=0) buffer databuf
+{
+    float data[];
+};
+
+void
+main()
+{
+    uint i = gl_GlobalInvocationID.x;
+    if(i < params.n) data[i] = params.val * i;
+}
+"""
+
+# Push constants are small packs of variables that are used to quickly send
+# configuration data to the shader runs. Make sure that this structure
+# corresponds to what is declared in the shader.
+struct ShaderPushConsts
+    val::Float32
+    n::UInt32
+end
+
+# Specialization constants are similar to push constants, but less dynamic: You
+# can change them before compiling the shader for the pipeline, but not
+# dynamically. This may have performance benefits for "very static" values,
+# such as block sizes.
+struct ShaderSpecConsts
+    local_size_x::UInt32
+end
+
+# Let's now compile the shader to SPIR-V with `glslang`. We can use the artifact `glslang_jll` which provides the binary through the [Artifact system](https://pkgdocs.julialang.org/v1/artifacts/).
+using glslang_jll: glslangValidator
+glslang = glslangValidator(identity)
+shader_bcode = mktempdir() do dir
+    inpath = joinpath(dir, "shader.comp")
+    outpath = joinpath(dir, "shader.spv")
+    open(f -> write(f, shader_code), inpath, "w")
+    status = run(`$glslang -V -S comp -o $outpath $inpath`)
+    @assert status.exitcode == 0
+    reinterpret(UInt32, read(outpath))
+end
+
+# We can now make a shader module with the compiled code:
+shader = ShaderModule(device, sizeof(UInt32) * length(shader_bcode), shader_bcode)
+
+# ## Assembling the pipeline
+#
+# Descriptor set layout describes how many resources of what kind are going to
+# be used by the shader. In this case, we only use a single buffer:
+dsl = DescriptorSetLayout(
+    device,
+    [
+        DescriptorSetLayoutBinding(
+            0,
+            DESCRIPTOR_TYPE_STORAGE_BUFFER,
+            SHADER_STAGE_COMPUTE_BIT;
+            descriptor_count = 1,
+        ),
+    ],
+)
+
+# Pipeline layout describes the descriptor set together with the location of
+# push constants:
+pl = PipelineLayout(
+    device,
+    [dsl],
+    [PushConstantRange(SHADER_STAGE_COMPUTE_BIT, 0, sizeof(ShaderPushConsts))],
+)
+
+# Shader compilation can use "specialization constants" that get propagated
+# (and optimized) into the shader code. We use them to make the shader
+# workgroup size "dynamic" in the sense that the size (32) is not hardcoded in
+# GLSL, but instead taken from here.
+const_local_size_x = 32
+spec_consts = [ShaderSpecConsts(const_local_size_x)]
+
+# Next, we create a pipeline that can run the shader code with the specified layout:
+pipeline_info = ComputePipelineCreateInfo(
+    PipelineShaderStageCreateInfo(
+        SHADER_STAGE_COMPUTE_BIT,
+        shader,
+        "main", # this needs to match the function name in the shader
+        specialization_info = SpecializationInfo(
+            [SpecializationMapEntry(0, 0, 4)],
+            UInt64(4),
+            Ptr{Nothing}(pointer(spec_consts)),
+        ),
+    ),
+    pl,
+    -1,
+)
+ps, _ = unwrap(create_compute_pipelines(device, [pipeline_info]))
+p = first(ps)
+
+# Now make a descriptor pool to allocate the buffer descriptors from (not a big
+# one, just 1 descriptor set with 1 descriptor in total), ...
+dpool = DescriptorPool(device, 1, [DescriptorPoolSize(DESCRIPTOR_TYPE_STORAGE_BUFFER, 1)])
+
+# ... allocate the descriptors for our layout, ...
+dsets = unwrap(allocate_descriptor_sets(device, DescriptorSetAllocateInfo(dpool, [dsl])))
+dset = first(dsets)
+
+# ... and make the descriptors point to the right buffers.
+update_descriptor_sets(
+    device,
+    [
+        WriteDescriptorSet(
+            dset,
+            0,
+            0,
+            DESCRIPTOR_TYPE_STORAGE_BUFFER,
+            [],
+            [DescriptorBufferInfo(buffer, 0, WHOLE_SIZE)],
+            [],
+        ),
+    ],
+    [],
+)
+
+# ## Executing the shader
+#
+# Let's create a command pool in the right queue family, and take a command
+# buffer out of that.
+cmdpool = CommandPool(device, qfam_idx)
+cbufs = unwrap(
+    allocate_command_buffers(
+        device,
+        CommandBufferAllocateInfo(cmdpool, COMMAND_BUFFER_LEVEL_PRIMARY, 1),
+    ),
+)
+cbuf = first(cbufs)
+
+# Now that we have a command buffer, we can fill it with commands that cause
+# the kernel to be run. Basically, we bind and fill everything, and then
+# dispatch a sufficient amount of invocations of the shader to span over the
+# array.
+begin_command_buffer(
+    cbuf,
+    CommandBufferBeginInfo(flags = COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT),
+)
+
+cmd_bind_pipeline(cbuf, PIPELINE_BIND_POINT_COMPUTE, p)
+
+const_buf = [ShaderPushConsts(1.234, data_items)]
+cmd_push_constants(
+    cbuf,
+    pl,
+    SHADER_STAGE_COMPUTE_BIT,
+    0,
+    sizeof(ShaderPushConsts),
+    Ptr{Nothing}(pointer(const_buf)),
+)
+
+cmd_bind_descriptor_sets(cbuf, PIPELINE_BIND_POINT_COMPUTE, pl, 0, [dset], [])
+
+cmd_dispatch(cbuf, div(data_items, const_local_size_x, RoundUp), 1, 1)
+
+end_command_buffer(cbuf)
+
+# Finally, find a handle to the compute queue and send the command to execute
+# the shader!
+compute_q = get_device_queue(device, qfam_idx, 0)
+unwrap(queue_submit(compute_q, [SubmitInfo([], [], [cbuf], [])]))
+
+# ## Getting the data
+#
+# After submitting the queue, the data is being crunched in the background. To
+# get the resulting data, we need to wait for completion and invalidate the
+# mapped memory (so that whatever data updates that happened on the GPU get
+# transferred to the mapped range visible for the host).
+#
+# While [`queue_wait_idle`](@ref) will wait for computations to be carried out,
+# we need to make sure that the required data is kept alive during queue
+# operations. In non-global scopes, such as functions, the compiler may skip
+# the allocation of unused variables or garbage-collect objects that the
+# runtime thinks are no longer used. If garbage-collected, objects will call
+# their finalizers which imply the destruction of the Vulkan objects
+# (via `vkDestroy...`). In this particular case, the runtime is not aware
+# that for example the pipeline and buffer objects are still used and that
+# there's a dependency with these variables until the command returns, so we
+# tell it manually.
+GC.@preserve buff dsl pl p const_buf begin
+    unwrap(queue_wait_idle(compute_q))
+end
+
+# Free the command buffers and the descriptor sets. These are the only handles that are not cleaned up automatically (see [Automatic finalization](@ref)).
+
+unwrap(free_command_buffers(device, cmdpool, cbufs))
+unwrap(free_descriptor_sets(device, dpool, dsets))
+
+# Just as with flushing, the invalidation is only required for memory that is
+# not host-coherent. You may skip this step if you check that the memory has
+# the host-coherent property flag.
+unwrap(invalidate_mapped_memory_ranges(device, [MappedMemoryRange(mem, 0, mem_size)]))
+
+# Finally, let's have a look at the data created by your compute shader!
+data # WHOA
