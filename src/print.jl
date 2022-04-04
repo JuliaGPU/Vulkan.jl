8c9b5a617b1222dd3df0f50d9215fd6e297ecf17
serenity4 <cedric.bel@hotmail.fr>
2022-04-04 23:31:47 +0200

    Add convenience function for PhysicalDeviceVulkan13Features

--- src/print.jl
+++ src/print.jl
@@ -4,13 +4,15 @@ end
 
 Base.show(io::IO, x::PhysicalDeviceMemoryProperties) = print(io, "PhysicalDeviceMemoryProperties($(x.memory_types[1:x.memory_type_count]), $(x.memory_heaps[1:x.memory_heap_count]))")
 
-function Base.show(io::IO, features::T) where {T<:Union{PhysicalDeviceFeatures,PhysicalDeviceVulkan11Features,PhysicalDeviceVulkan12Features}}
+const PhysicalDeviceVulkanFeatures_T = Union{PhysicalDeviceVulkan11Features,PhysicalDeviceVulkan12Features,PhysicalDeviceVulkan13Features}
+
+function Base.show(io::IO, features::T) where {T<:Union{PhysicalDeviceFeatures,PhysicalDeviceVulkanFeatures_T}}
     fnames = fieldnames(typeof(features))
     fields = filter(!in((:s_type, :next)), fnames)
     vals = Bool.(getproperty.(Ref(features), fields))
     print(io, nameof(T), '(')
     idxs = findall(vals)
-    if features isa PhysicalDeviceVulkan11Features || features isa PhysicalDeviceVulkan12Features
+    if features isa PhysicalDeviceVulkanFeatures_T
         print(io, "next=", features.next)
         !isempty(idxs) && print(io, ", ")
     end
