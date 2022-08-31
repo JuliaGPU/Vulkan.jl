c1e90b61a6b6544f4674ed5cbc571c7065f4ff87
serenity4 <cedric.bel@hotmail.fr>
2022-08-31 19:30:10 +0200

    Revert to request Vulkan 1.3, mark test as broken on CI

--- test/base.jl
+++ test/base.jl
@@ -1,6 +1,7 @@
 # set libname for the loader (must be done before importing Vulkan)
 # this is mandatory in cases where the default libname used by VulkanCore does not point to a valid Vulkan library
-if get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "ON"
+is_ci = get(ENV, "JULIA_GITHUB_ACTIONS_CI", "OFF") == "ON"
+if is_ci
     import SwiftShader_jll
     ENV["JULIA_VULKAN_LIBNAME"] = basename(SwiftShader_jll.libvulkan)
 end
@@ -18,7 +19,7 @@ end
 include("bitmasks.jl")
 include("api.jl")
 include("dispatch.jl")
-@test Vulkan.precompile_workload()
+@test Vulkan.precompile_workload() broken = is_ci
 
 DocMeta.setdocmeta!(Vulkan, :DocTestSetup, quote
     using Vulkan
