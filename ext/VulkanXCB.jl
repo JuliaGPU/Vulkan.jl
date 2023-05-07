905c711e67d86e91903a8c4fb6e66198f7c974bd
serenity4 <cedric.bel@hotmail.fr>
2023-05-07 15:15:03 +0200

    Add package extension for XCB

---
+++ ext/VulkanXCB.jl
@@ -0,0 +1,10 @@
+module VulkanXCB
+
+using Vulkan
+using XCB: XCBWindow
+
+function Vk.SurfaceKHR(instance, window::XCBWindow)
+  unwrap(Vk.create_xcb_surface_khr(instance, Vk.XcbSurfaceCreateInfoKHR(window.conn.h, window.id)))
+end
+
+end
