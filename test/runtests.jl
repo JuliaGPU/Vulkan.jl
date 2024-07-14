1e0367eed81103a6532a9c3aa2bb2432dd76863c
serenity4 <cedric.bel@hotmail.fr>
2024-07-14 13:13:19 +0200

    Attempt to install Lavapipe

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
 # Set libname for the loader (must be done before importing Vulkan).
 # This is mandatory in cases where the default libname used by VulkanCore does not point to a valid Vulkan library.
-import SwiftShader_jll
-ENV["JULIA_VULKAN_LIBNAME"] = basename(SwiftShader_jll.libvulkan)
+# import SwiftShader_jll
+# ENV["JULIA_VULKAN_LIBNAME"] = basename(SwiftShader_jll.libvulkan)
 
 using Test
 using Vulkan
 using Accessors: @set
 
 # Use SwiftShader for testing.
-set_driver(:SwiftShader)
+# set_driver(:SwiftShader)
 
 @testset "Vulkan.jl" begin
     include("handles.jl")
